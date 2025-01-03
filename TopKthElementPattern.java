a4e2792084785813e0fb60dff7dd3c0d80a8b5af
Sandesh Sonaba Khilari <sandeshsonabakhilari@Sandeshs-MacBook-Pro.local>
2025-01-04 00:02:12 +0530

    File added Successfully

--- TopKthElementPattern.java
+++ TopKthElementPattern.java
@@ -1,3 +1,19 @@
+import java.util.*;
 class TopKthElementPattern{
-
+  public static int greatorKth(int[] nums, int k){
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    for(int num : nums){
+      minHeap.offer(num);
+      if(minHeap.size()>k){
+        minHeap.poll();
+      }
+    }
+    return minHeap.peek();
+  }
+  public static void main(String args[]){
+    int nums[] = {3, 2, 1, 5, 6};
+    int k = 1;
+    int ans = greatorKth(nums, k);
+    System.out.println(ans);
+  }
 }
